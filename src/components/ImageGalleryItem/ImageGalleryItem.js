2d3a8b3ce404ba7c19e12b1a18ea34734465e950
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2023-11-10 20:44:48 +0200

    test

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -3,7 +3,7 @@ import { GalleryItem } from './ImageGalleryItem.styled';
 export const ImageGalleryItem = ({ webformatURL, largeImageURL }) => {
   return (
     <GalleryItem>
-      <img src={webformatURL} alt="Image by your request" />
+      <img src={webformatURL} alt="by your request" />
     </GalleryItem>
   );
 };
