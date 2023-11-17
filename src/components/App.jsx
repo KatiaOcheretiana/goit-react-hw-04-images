cc32371e909b3882b2094478188b8a1f31143f39
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2023-11-17 11:51:57 +0200

    used hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { searchByName } from 'helper/api-request';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -7,87 +7,158 @@ import { Loader } from './Loader/Loader';
 import toast, { Toaster } from 'react-hot-toast';
 import { nanoid } from 'nanoid';
 
-export class App extends Component {
-  state = {
-    searchItem: '',
-    randomId: 0,
-    images: [],
-    page: 1,
-    perPage: 12,
-    isLoad: false,
-    error: false,
-    availablePages: 0,
-  };
+export const App = () => {
+  const [searchItem, setSearchItem] = useState('');
+  const [randomId, setRandomId] = useState(0);
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoad, setIsLoad] = useState(false);
+  const [error, setError] = useState(false);
+  const [availablePages, setAvailablePages] = useState(0);
+  const perPage = 12;
 
-  async componentDidUpdate(prevProps, prevState) {
-    const { searchItem, page, perPage, randomId } = this.state;
+  useEffect(() => {
+    const fetchData = async () => {
+      if (searchItem === '') {
+        return;
+      }
 
-    if (
-      prevState.searchItem !== searchItem ||
-      prevState.page !== page ||
-      prevState.randomId !== randomId
-    ) {
       try {
-        this.setState({ isLoad: true, error: false });
+        setError(false);
+        setIsLoad(true);
         const imagesData = await searchByName(searchItem, page);
 
         if (imagesData.hits.length > 0) {
-          this.setState(prevState => {
-            return {
-              images: [...prevState.images, ...imagesData.hits],
-              availablePages: Math.ceil(imagesData.totalHits / perPage),
-            };
-          });
+          setImages(prev => [...prev, ...imagesData.hits]);
+          setAvailablePages(Math.ceil(imagesData.totalHits / perPage));
           toast.success('Successfully found!');
         } else {
           toast.error('Nothing found.');
         }
       } catch (error) {
-        this.setState({ error: true });
+        setError(true);
       } finally {
-        this.setState({ isLoad: false });
+        setIsLoad(false);
       }
-    }
-  }
-
-  onSubmitSearch = searchName => {
-    this.setState({
-      searchItem: searchName,
-      page: 1,
-      images: [],
-      randomId: nanoid(),
-    });
-  };
-
-  onLoadMore = () => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
-    });
-  };
-
-  render() {
-    const containerStyle = {
-      display: 'grid',
-      gridTemplateColumns: '1fr',
-      gridGap: '16px',
-      paddingBottom: '24px',
     };
+    fetchData();
+  }, [searchItem, page, randomId]);
 
-    const { isLoad, images, error, page, availablePages } = this.state;
+  const onSubmitSearch = searchName => {
+    setSearchItem(searchName);
+    setPage(1);
+    setImages([]);
+    setRandomId(nanoid());
+  };
 
-    return (
-      <div style={containerStyle}>
-        <Searchbar onSubmit={this.onSubmitSearch} />
-        {error && (
-          <b>Oops! Something went wrong! Please try reloading this page! 🥹</b>
-        )}
-        {isLoad && <Loader />}
-        {images.length > 0 && <ImageGallery items={images} />}
-        {page !== availablePages && images.length !== 0 && (
-          <Button onClick={this.onLoadMore} />
-        )}
-        <Toaster />
-      </div>
-    );
-  }
-}
+  const onLoadMore = () => {
+    setPage(prev => prev + 1);
+  };
+
+  const containerStyle = {
+    display: 'grid',
+    gridTemplateColumns: '1fr',
+    gridGap: '16px',
+    paddingBottom: '24px',
+  };
+
+  return (
+    <div style={containerStyle}>
+      <Searchbar onSubmit={onSubmitSearch} />
+      {error && (
+        <b>Oops! Something went wrong! Please try reloading this page! 🥹</b>
+      )}
+      {isLoad && <Loader />}
+      {images.length > 0 && <ImageGallery items={images} />}
+      {page !== availablePages && images.length !== 0 && (
+        <Button onClick={onLoadMore} />
+      )}
+      <Toaster />
+    </div>
+  );
+};
+
+// export class App extend Component {
+//   state = {
+//     searchItem: '',
+//     randomId: 0,
+//     images: [],
+//     page: 1,
+//     perPage: 12,
+//     isLoad: false,
+//     error: false,
+//     availablePages: 0,
+//   };
+
+//   async componentDidUpdate(prevProps, prevState) {
+//     const { searchItem, page, perPage, randomId } = this.state;
+
+//     if (
+//       prevState.searchItem !== searchItem ||
+//       prevState.page !== page ||
+//       prevState.randomId !== randomId
+//     ) {
+//       try {
+//         this.setState({ isLoad: true, error: false });
+//         const imagesData = await searchByName(searchItem, page);
+
+//         if (imagesData.hits.length > 0) {
+//           this.setState(prevState => {
+//             return {
+//               images: [...prevState.images, ...imagesData.hits],
+//               availablePages: Math.ceil(imagesData.totalHits / perPage),
+//             };
+//           });
+//           toast.success('Successfully found!');
+//         } else {
+//           toast.error('Nothing found.');
+//         }
+//       } catch (error) {
+//         this.setState({ error: true });
+//       } finally {
+//         this.setState({ isLoad: false });
+//       }
+//     }
+//   }
+
+//   onSubmitSearch = searchName => {
+//     this.setState({
+//       searchItem: searchName,
+//       page: 1,
+//       images: [],
+//       randomId: nanoid(),
+//     });
+//   };
+
+//   onLoadMore = () => {
+//     this.setState(prevState => {
+//       return { page: prevState.page + 1 };
+//     });
+//   };
+
+//   render() {
+//     const containerStyle = {
+//       display: 'grid',
+//       gridTemplateColumns: '1fr',
+//       gridGap: '16px',
+//       paddingBottom: '24px',
+//     };
+
+//     const { isLoad, images, error, page, availablePages } = this.state;
+
+//     return (
+//       <div style={containerStyle}>
+//         <Searchbar onSubmit={this.onSubmitSearch} />
+//         {error && (
+//           <b>Oops! Something went wrong! Please try reloading this page! 🥹</b>
+//         )}
+//         {isLoad && <Loader />}
+//         {images.length > 0 && <ImageGallery items={images} />}
+//         {page !== availablePages && images.length !== 0 && (
+//           <Button onClick={this.onLoadMore} />
+//         )}
+//         <Toaster />
+//       </div>
+//     );
+//   }
+// }
--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,29 +1,49 @@
 import { Modal } from 'components/Modal/Modal';
 import { GalleryItem } from './ImageGalleryItem.styled';
-import { Component } from 'react';
+import { useState } from 'react';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    isModalOpen: false,
+export const ImageGalleryItem = ({ webformatURL, largeImageURL, tags }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleStateOfModal = () => {
+    setIsModalOpen(prev => !prev);
   };
 
-  toggleStateOfModal = () => {
-    this.setState({ isModalOpen: !this.state.isModalOpen });
-  };
+  return (
+    <GalleryItem>
+      <img src={webformatURL} alt={tags} onClick={toggleStateOfModal} />
+      <Modal
+        url={largeImageURL}
+        isModalOpen={isModalOpen}
+        toggleStateOfModal={toggleStateOfModal}
+        alt={tags}
+      />
+    </GalleryItem>
+  );
+};
 
-  render() {
-    const { webformatURL, largeImageURL, tags } = this.props;
-    const { isModalOpen } = this.state;
-    return (
-      <GalleryItem>
-        <img src={webformatURL} alt={tags} onClick={this.toggleStateOfModal} />
-        <Modal
-          url={largeImageURL}
-          isModalOpen={isModalOpen}
-          toggleStateOfModal={this.toggleStateOfModal}
-          alt={tags}
-        />
-      </GalleryItem>
-    );
-  }
-}
+// export class ImageGalleryItem extends Component {
+//   state = {
+//     isModalOpen: false,
+//   };
+
+//   toggleStateOfModal = () => {
+//     this.setState({ isModalOpen: !this.state.isModalOpen });
+//   };
+
+//   render() {
+//     const { webformatURL, largeImageURL, tags } = this.props;
+//     const { isModalOpen } = this.state;
+//     return (
+//       <GalleryItem>
+//         <img src={webformatURL} alt={tags} onClick={this.toggleStateOfModal} />
+//         <Modal
+//           url={largeImageURL}
+//           isModalOpen={isModalOpen}
+//           toggleStateOfModal={this.toggleStateOfModal}
+//           alt={tags}
+//         />
+//       </GalleryItem>
+//     );
+//   }
+// }
