15d067ecb81d6cec84d40901df4e10bbc9f187f1
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2023-11-18 18:53:46 +0200

    fixed

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -21,29 +21,3 @@ export const ImageGalleryItem = ({ webformatURL, largeImageURL, tags }) => {
     </GalleryItem>
   );
 };
-
-// export class ImageGalleryItem extends Component {
-//   state = {
-//     isModalOpen: false,
-//   };
-
-//   toggleStateOfModal = () => {
-//     this.setState({ isModalOpen: !this.state.isModalOpen });
-//   };
-
-//   render() {
-//     const { webformatURL, largeImageURL, tags } = this.props;
-//     const { isModalOpen } = this.state;
-//     return (
-//       <GalleryItem>
-//         <img src={webformatURL} alt={tags} onClick={this.toggleStateOfModal} />
-//         <Modal
-//           url={largeImageURL}
-//           isModalOpen={isModalOpen}
-//           toggleStateOfModal={this.toggleStateOfModal}
-//           alt={tags}
-//         />
-//       </GalleryItem>
-//     );
-//   }
-// }
